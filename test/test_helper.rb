659de81755f7c23235e10d3c543ee8d7b894c39e
trinh7293 <trinh7293@gmail.com>
2017-04-20 15:41:43 +0700

    Implement advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../../config/environment", __FILE__)
+require "rails/test_help"
 require "minitest/reporters"
 Minitest::Reporters.use!
 
@@ -12,4 +12,16 @@ class ActiveSupport::TestCase
     session[:user_id].present?
   end
 
+  def log_in_as user
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+
+  def log_in_as user, password: "password", remember_me: "1"
+    post login_path, params: {session: {email: user.email,
+      password: password,
+      remember_me: remember_me}}
+  end
 end
